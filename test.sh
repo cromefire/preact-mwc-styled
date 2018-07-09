8e95d845cca3df7cb804816c35805c0f85e5f576
Cromefire_ <tim.l@nghorst.net>
2018-07-09 15:09:04 +0200

    improved travis build
    Signed-off-by: Cromefire_ <cromefirehd@gmail.com>

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-echo "Linting typescript..."
-tslint -p tsconfig.json -e "node_modules/**/*"
+
+echo "Linting TypeScript"
+tslint -p tsconfig.json -e \"node_modules/**/*\"
+status=$?
+
+if [ ${status} -ne 0 ]; then
+    exit 1;
+fi
