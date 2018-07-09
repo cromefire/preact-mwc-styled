e0f980cdd87cce63e8a09e18fc54ca37ff646c45
Cromefire_ <tim.l@nghorst.net>
2018-07-09 14:34:28 +0200

    Add the Button component and basic build stuff (#1)
    * added styled button
    
    Signed-off-by: Cromefire_ <cromefirehd@gmail.com>
    
    * moved to index.ts for better importing
    
    Signed-off-by: Cromefire_ <cromefirehd@gmail.com>
    
    * excluded artifacts from git
    
    Signed-off-by: Cromefire_ <cromefirehd@gmail.com>
    
    * excluded artifacts more from git
    
    Signed-off-by: Cromefire_ <cromefirehd@gmail.com>
    
    * Added documentation
    
    Signed-off-by: Cromefire_ <cromefirehd@gmail.com>
    
    * Upgraded packages
    
    Signed-off-by: Cromefire_ <cromefirehd@gmail.com>

---
+++ util/style.ts
@@ -0,0 +1,60 @@
+const counts: { [id: string]: number } = {};
+
+function use(id) {
+    if (!counts.hasOwnProperty(id)) {
+        counts[id] = 1;
+        return true;
+    } else {
+        counts[id]++;
+        return counts[id] <= 1;
+    }
+}
+
+function unuse(id) {
+    if (!counts.hasOwnProperty(id)) {
+        counts[id] = 0;
+        return true;
+    } else {
+        counts[id]--;
+        return counts[id] <= 0;
+    }
+}
+
+export function load_styles(styles: Array<{ style: string | { use?: () => void }, id: string }>) {
+    for (const style_def of styles) {
+        if (typeof style_def.style === "string") {
+            let elem = document.getElementById(`${style_def.id}_style`);
+            if (!elem) {
+                elem = document.createElement("style");
+                elem.id = `${style_def.id}_style`;
+            }
+            if (use(style_def.id)) {
+                elem.innerText = "";
+                elem.appendChild(document.createTextNode(style_def.style));
+            }
+        } else {
+            if (style_def.style.use) {
+                style_def.style.use();
+            }
+        }
+    }
+}
+
+export function unload_styles(styles: Array<{ style: string | { unuse?: () => void }, id: string }>) {
+    for (const style_def of styles) {
+        if (typeof style_def.style === "string") {
+            let elem = document.getElementById(`${style_def.id}_style`);
+            if (!elem) {
+                elem = document.createElement("style");
+                elem.id = `${style_def.id}_style`;
+            }
+            if (unuse(style_def.id)) {
+                elem.innerText = "";
+            }
+        } else {
+            if (style_def.style.unuse) {
+                style_def.style.unuse();
+            }
+        }
+    }
+}
