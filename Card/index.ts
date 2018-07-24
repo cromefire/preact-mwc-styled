841658dfe654b697938862ae08d5fdf3c6fbd515
Cromefire_ <tim.l@nghorst.net>
2018-07-24 14:27:39 +0200

    Added card component
    Signed-off-by: CromefireHD <cromefirehd@gmail.com>

---
+++ Card/index.ts
@@ -0,0 +1,22 @@
+import PCard from "preact-material-components/Card/Card";
+import {load_styles, unload_styles} from "../util/style";
+import card_style from "./card.scss";
+
+export class Card extends PCard {
+    public readonly styles = [
+        {style: card_style, id: "card"}
+    ];
+
+    public componentWillMount() {
+        super.componentWillMount();
+        load_styles(this.styles);
+    }
+
+    public componentWillUnmount() {
+        super.componentWillUnmount();
+        unload_styles(this.styles);
+    }
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default Card;
