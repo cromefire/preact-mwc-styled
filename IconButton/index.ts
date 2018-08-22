0de58349183b95b611b4098af593b34eabb304f7
Cromefire_ <tim.l@nghorst.net>
2018-08-22 16:00:08 +0200

    Renamed base interfaces
    Signed-off-by: Cromefire_ <tim.l@nghorst.net>

--- IconButton/index.ts
+++ IconButton/index.ts
@@ -1,11 +1,14 @@
-import RIconButton from "preact-material-components/esm/IconButton";
+import RIconButton, {
+    IIconButtonProps as RIconButtonProps,
+    IIconButtonState as RIconButtonState
+} from "preact-material-components/esm/IconButton";
 import {load_styles, unload_styles} from "../util/style";
 import style from "./index.scss";
 
-export interface IIconButtonProps extends IIconButtonProps {
+export interface IIconButtonProps extends RIconButtonProps {
 }
 
-export interface IIconButtonState extends IIconButtonState {
+export interface IIconButtonState extends RIconButtonState {
 }
 
 export class IconButton extends RIconButton {
